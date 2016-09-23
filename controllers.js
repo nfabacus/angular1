3dd727400ed3c71b16b5ba1e0defc321ea165586
6eff <6eff@mail.ru>
2016-09-23 13:09:56 +0300

    adds addToDo and removeToDo exp to comtroller

--- controllers.js
+++ controllers.js
@@ -1,4 +1,14 @@
 toDoApp.controller('ToDoController', function() {
   // two objects, each with two attributes
-  this.todos = [{ task: "ToDo1", completed: true }, { task: "ToDo2", completed: false }];
+  var self = this;
+  self.todos = [{ task: "ToDo1", completed: true }, { task: "ToDo2", completed: false }];
+  self.addToDo = function(todoText){
+    self.todos.push ({
+      text: todoText, completed: false
+    });
+  };
+
+  self.removeToDo = function(){
+    self.todos.pop();
+  };
 });
